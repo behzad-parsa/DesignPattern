734a86ebe1280def9ed002a5b79c94564c8f1c89
Behzad Parsa <behzad.afb2012@yahoo.com>
2023-03-25 15:15:17 +0330

    Add project files.

---
+++ MementoPattern/Editor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    internal class Editor
+    {
+        private string content;
+        private readonly Stack<string> stack;
+        
+        public string Content { 
+            get
+            {
+                return content;
+            }
+            set
+            {
+                stack.Push(value);
+                content = stack.Peek();
+            }
+        }
+
+
+        public Editor()
+        {
+            stack = new Stack<string>();
+        }
+
+        public void Undo()
+        {
+            stack.Pop();
+            content = stack.Peek();
+        }
+    }
+}
---
+++ MementoPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Editor editor = new Editor();
+            editor.Content = "a";
+            editor.Content = "b";
+            editor.Content = "c";
+            Console.WriteLine(editor.Content);
+            editor.Undo();
+            Console.WriteLine(editor.Content);
+            editor.Undo();
+            Console.WriteLine(editor.Content);
+
+            Console.ReadLine();
+        }
+    }
+}
