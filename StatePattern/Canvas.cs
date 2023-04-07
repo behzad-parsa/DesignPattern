811953b3e136a25fd2917c5befd9d289ddbbec84
Behzad Parsa <behzad.afb2012@yahoo.com>
2023-04-07 17:05:49 +0330

    State Pattern - Implementation

---
+++ StatePattern/Canvas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    internal class Canvas
+    {
+        public ITool CurrentTool { get; set; }
+
+        public void MouseDown()
+        {
+            CurrentTool.MouseDown();   
+        }
+        public void MouseUp()
+        {
+            CurrentTool.MouseUp();
+        }
+    }
+}
---
+++ StatePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var canvas = new Canvas();
+            canvas.CurrentTool = new SelectionTool();
+            canvas.MouseDown();
+            canvas.MouseUp();
+            
+            canvas.CurrentTool = new BrushTool();
+            canvas.MouseDown();
+            canvas.MouseUp();
+
+
+            Console.ReadLine();
+        }
+    }
+}
